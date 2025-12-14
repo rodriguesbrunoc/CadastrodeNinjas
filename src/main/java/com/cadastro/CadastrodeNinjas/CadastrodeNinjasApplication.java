b876dcc241270bfd4aadb6ebe603870597ffbbc5
Bruno Rodrigues <bruno.dev.rodrigues@gmail.com>
2025-12-14 19:22:19 -0300

    first commit

---
+++ src/main/java/com/cadastro/CadastrodeNinjas/CadastrodeNinjasApplication.java
@@ -0,0 +1,13 @@
+package com.cadastro.CadastrodeNinjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrodeNinjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrodeNinjasApplication.class, args);
+	}
+
+}
